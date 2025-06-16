1e070a0ca5b80d65369b6804e1b93f9a7674af4d
Kiruba1018 <kirubav3vva155@gmail.com>
2025-06-16 21:38:22 +0530

    Natural Numbers

---
+++ natural.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class natural {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    nat(n);
+  }
+  public static void nat(int n){
+    for(int i=1;i<=n;i++){
+      System.out.println(i);
+    }
+  }
+}
